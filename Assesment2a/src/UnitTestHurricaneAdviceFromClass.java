fe0b0b6f2b28d1e9f07b790fc7401f3d2ca645b3
JSDale <jdale994@googlemail.com>
2020-01-02 11:07:11 +0000

    UnitTestHurricaneAdviceFromClass has been completed and passed.

--- Assesment2a/src/UnitTestHurricaneAdviceFromClass.java
+++ Assesment2a/src/UnitTestHurricaneAdviceFromClass.java
@@ -7,11 +7,44 @@ class UnitTestHurricaneAdviceFromClass
     Storm hurricane = new Huricane();
 
     @Test
+    void AdviceTropDep()
+    {
+        assertEquals("Prepare for a possible Hurricane", hurricane.GetAdvice("Tropical Depression"));
+    }
+
+    @Test
+    void AdviceTropStorm()
+    {
+        assertEquals("Close storm shutters and stay away from windows", hurricane.GetAdvice("Tropical Storm"));
+    }
+
+    @Test
+    void AdviceCat1()
+    {
+        assertEquals("Close storm shutters and stay away from windows", hurricane.GetAdvice("Category 1"));
+    }
+
+    @Test
+    void AdviceCat2()
+    {
+        assertEquals("Close storm shutters and stay away from windows", hurricane.GetAdvice("Category 2"));
+    }
+
+    @Test
+    void AdviceCat3()
+    {
+        assertEquals("Coastal regions are encouraged to evacuate", hurricane.GetAdvice("Category 3"));
+    }
+
+    @Test
+    void AdviceCat4()
+    {
+        assertEquals("Evacuate", hurricane.GetAdvice("Category 4"));
+    }
+
+    @Test
     void AdviceCat5()
     {
-        hurricane.setStormName("testTornado");
-        hurricane.setClassification("Category 5");
-
         assertEquals("Evacuate", hurricane.GetAdvice("Category 5"));
     }
 }
